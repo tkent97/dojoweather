c43831933661f41b824d1ff6f12cc4473d27572c
tkent97 <tristen123abc@gmail.com>
2021-06-30 20:59:00 -0700

    initial commit

---
+++ dojo weather/script.js
@@ -0,0 +1,21 @@
+function loadreport() {
+    return alert("Loading weather report");
+}
+
+function hide(element) {
+    console.log(element);
+    console.log(element.parentNode);
+    return element.parentNode.style.display = "none";
+}
+
+function ctof() {
+
+    document.getElementsByClassName("temp")
+    // iterate through the array
+    for (var i = "24"; i < "75"; i++)
+        if array[i].innerText == "24°"{
+            // return array[i].innerText = "75°";
+            // }else if{
+            array[i].innerText = "24°"
+        }
+}
